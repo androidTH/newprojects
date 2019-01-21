b6142ba23b8a63804254f134080d2515f4697e7e
“金家瑞” <jinjiarui@newscctv.cn>
2019-01-21 22:00:29 +0800

    1月21号，api修改

--- app/src/main/java/com/d6/android/app/net/API.kt
+++ app/src/main/java/com/d6/android/app/net/API.kt
@@ -4,9 +4,16 @@ package com.d6.android.app.net
  * Created on 2017/12/27.
  */
 object API {
-//    const val BASE_URL = "http://47.105.50.76/JyPhone/"
-    const val BASE_URL = "http://www.d6-zone.com/JyPhone/"
-//    const val STATIC_BASE_URL = "http://47.105.50.76/JyPhone/#/"
-    const val STATIC_BASE_URL = "http://www.d6-zone.com/JyD6/#/"
 
+    /**
+     * 测试环境
+     */
+    const val BASE_URL = "http://47.105.50.76/JyPhone/"
+    const val STATIC_BASE_URL = "http://47.105.50.76/JyPhone/#/"
+
+    /**
+     * 正式环境
+     */
+//    const val BASE_URL = "http://www.d6-zone.com/JyPhone/"
+//    const val STATIC_BASE_URL = "http://www.d6-zone.com/JyD6/#/"
 }
