b5447fdccb08e85ac33d3b68050f43e0cf08071a
“金家瑞” <jinjiarui@newscctv.cn>
2019-07-14 15:47:16 +0800

    d6 2.3.0 发布动态ui

--- app/src/main/java/com/d6/android/app/activities/UserInfoActivity.kt
+++ app/src/main/java/com/d6/android/app/activities/UserInfoActivity.kt
@@ -452,7 +452,7 @@ class UserInfoActivity : BaseActivity(), SwipeRefreshRecyclerLayout.OnRefreshLis
                         headerView.img_date_auther.backgroundDrawable=ContextCompat.getDrawable(context,R.mipmap.video_small)
                     }
 
-                    if(TextUtils.equals("0",it.isValid)){
+                    if(TextUtils.equals("0",it.isValid)||TextUtils.equals("2",it.isValid)){
                         headerView.img_other_auther.visibility = View.GONE
                         headerView.img_date_auther.visibility = View.VISIBLE
                         headerView.rl_prompt.visibility = View.VISIBLE
