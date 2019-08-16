982c7ef3b552b76e853312f07b829be83f0c6f93
“金家瑞” <jinjiarui@newscctv.cn>
2019-08-16 16:45:55 +0800

    d6Dev 收益名字高亮

--- app/src/main/java/com/d6/android/app/adapters/PointsAdapter.kt
+++ app/src/main/java/com/d6/android/app/adapters/PointsAdapter.kt
@@ -16,29 +16,29 @@ import org.jetbrains.anko.textColor
 /**
  *粉丝
  */
-class PointsAdapter(mData:ArrayList<UserPoints>): HFRecyclerAdapter<UserPoints>(mData,  R.layout.item_list_mypoints){//R.layout.item_list_mypoints
+class PointsAdapter(mData: ArrayList<UserPoints>) : HFRecyclerAdapter<UserPoints>(mData, R.layout.item_list_mypoints) {//R.layout.item_list_mypoints
 
     //1、充值积分  2、消费积分 3、充值购买小红花 4、提现小红花 5、收到小红花 6、赠送小红花  3、5是加  4、6是减
     override fun onBind(holder: ViewHolder, position: Int, data: UserPoints) {
-        var  tv_mypoints_content= holder.bind<TextView>(R.id.tv_mypoints_content)
+        var tv_mypoints_content = holder.bind<TextView>(R.id.tv_mypoints_content)
         val tv_mypointstime = holder.bind<TextView>(R.id.tv_mypointstime)
         tv_mypointstime.text = data.dCreatetime.toTime("yyyy.MM.dd HH:mm")
         val tv_nums = holder.bind<TextView>(R.id.tv_nums)
-        if(data.iPointtype == 1||data.iPointtype==3||data.iPointtype == 5){
+        if (data.iPointtype == 1 || data.iPointtype == 3 || data.iPointtype == 5) {
             tv_nums.text = "+${data.iPoint.toString()}"
-            tv_nums.textColor = ContextCompat.getColor(context,R.color.color_F7AB00)
-        }else if(data.iPointtype == 2||data.iPointtype == 4|| data.iPointtype == 6){
+            tv_nums.textColor = ContextCompat.getColor(context, R.color.color_F7AB00)
+        } else if (data.iPointtype == 2 || data.iPointtype == 4 || data.iPointtype == 6) {
             tv_nums.text = "-${data.iPoint.toString()}"
-            tv_nums.textColor =ContextCompat.getColor(context,R.color.color_68BFFF)
+            tv_nums.textColor = ContextCompat.getColor(context, R.color.color_68BFFF)
         }
 
-        if(data.iType==16||data.iType==17||data.iType==18){
+        if (data.iType == 16 || data.iType == 17 || data.iType == 18) {
             data.sPointdesc?.let {
                 var index = it.indexOf("@${data.sSendUserName}")
-                var str = it.replace("@${data.sSendUserName}","${data.sSendUserName}")
+                var str = it.replace("@${data.sSendUserName}", "${data.sSendUserName}")
                 var span = SpannableStringBuilder(str)
-                var len = (index+data.sSendUserName.length)
-                span.setSpan(ForegroundColorSpan(ContextCompat.getColor(context,R.color.color_F7AB00)), index, len, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+                var len = (index + data.sSendUserName.length)
+                span.setSpan(ForegroundColorSpan(ContextCompat.getColor(context, R.color.color_F7AB00)), index, len, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
                 tv_mypoints_content.text = span
             }
         }
