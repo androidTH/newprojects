6ccbe844c60b2a5f5300c101f50be21ac2c985f8
“金家瑞” <jinjiarui@newscctv.cn>
2019-11-01 22:33:51 +0800

    2.12.0 弹幕分页显示

--- app/src/main/java/com/d6/android/app/base/BaseActivity.kt
+++ app/src/main/java/com/d6/android/app/base/BaseActivity.kt
@@ -24,7 +24,7 @@ import java.lang.Exception
 import android.view.MotionEvent
 import android.view.View
 import android.view.WindowManager
-//import com.bugtags.library.Bugtags
+import com.bugtags.library.Bugtags
 import com.d6.android.app.utils.KeyboardktUtils
 import com.d6.android.app.widget.LoadDialog
 import com.gyf.barlibrary.ImmersionBar
@@ -92,13 +92,13 @@ abstract class BaseActivity : AppCompatActivity(), AnkoLogger, RequestManager {
     override fun onResume() {
         super.onResume()
         MobclickAgent.onResume(this)
-//        Bugtags.onResume(this)
+        Bugtags.onResume(this)
     }
 
     override fun onPause() {
         super.onPause()
         MobclickAgent.onPause(this)
-//        Bugtags.onPause(this)
+        Bugtags.onPause(this)
     }
 
     override fun onDestroy() {
@@ -129,7 +129,7 @@ abstract class BaseActivity : AppCompatActivity(), AnkoLogger, RequestManager {
 
     override fun dispatchTouchEvent(event: MotionEvent): Boolean {
         //注：回调 3
-//        Bugtags.onDispatchTouchEvent(this, event)
+        Bugtags.onDispatchTouchEvent(this, event)
         return super.dispatchTouchEvent(event)
     }
 
