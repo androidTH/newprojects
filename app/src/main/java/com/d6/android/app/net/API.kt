1304d8839d7245d6126eb5b8ec2978577b6e2e41
“金家瑞” <jinjiarui@newscctv.cn>
2019-11-11 15:22:25 +0800

    3.0.0 正式包

--- app/src/main/java/com/d6/android/app/net/API.kt
+++ app/src/main/java/com/d6/android/app/net/API.kt
@@ -11,8 +11,8 @@ object API {
     /**
      * 测试环境
      */
-    const val BASE_URL = "http://47.105.50.76/JyPhone/"
-    const val STATIC_BASE_URL = "http://47.105.50.76/JyPhone/#/"
+//    const val BASE_URL = "http://47.105.50.76/JyPhone/"
+//    const val STATIC_BASE_URL = "http://47.105.50.76/JyPhone/#/"
 
     /**
      * 正式环境
