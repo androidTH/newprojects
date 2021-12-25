66ea2578f93d2bf4e52a6a5e1d43c7f3746e94c3
“金家瑞” <jinjiarui@newscctv.cn>
2021-12-25 11:19:06 +0800

    修改版本号

---
+++ CallKit/build/generated/source/buildConfig/release/io/rong/callkit/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package io.rong.callkit;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "io.rong.callkit";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 20201225;
+  public static final String VERSION_NAME = "4.0.3.6_hotfix/4.0.3";
+}
